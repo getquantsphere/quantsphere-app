bbb2d16df401de5781dd2ef4ae60f7776b06bfc0
dedsecrattle <itsprabxxx@gmail.com>
2024-02-10 03:20:04 +0800

    Authentication Fixed

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -35,19 +34,13 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: const Text(
-          "Landing Page of the application"), // This trailing comma makes auto-formatting nicer for build methods.
+          "Landing Page of the application"),
     );
   }
 }
